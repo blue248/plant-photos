ca620e631c76e1cb6f3dc35371d987f3a60f7c9b
blue248 <a.k0reha.mar1m0@gmail.com>
2023-01-17 23:16:09 +0900

    編集ページへのアクションとビュー作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,13 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+  end
+
   private
   def post_params
     params.require(:post).permit(:image, :content).merge(user_id: current_user.id)
