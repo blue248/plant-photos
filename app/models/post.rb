d3fcd27dd8fed3addfbdb5da885319e00069effb
blue248 <a.k0reha.mar1m0@gmail.com>
2023-01-19 23:23:14 +0900

    tagsテーブルとpost_tag_relationテーブルの作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :comments
+  has_many :post_tag_relations
+  has_many :tags, through: :post_tag_relations
 end
