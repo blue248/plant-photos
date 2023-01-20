c32efea12ac0dbf14d522e3aa0a56e63a5defb7a
blue248 <a.k0reha.mar1m0@gmail.com>
2023-01-20 10:35:54 +0900

    投稿削除すると同時にコメントも削除される

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :post_tag_relations, dependent: :destroy
   has_many :tags, through: :post_tag_relations
 end
