efc6a983797a42f4a7908383a3e08cf2da7163bc
blue248 <a.k0reha.mar1m0@gmail.com>
2023-01-26 17:28:35 +0900

    input要素で値の変化が起きた時に呼び出される関数を定義

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -7,5 +7,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // 新規投稿・編集ページのフォームがないならここで終了。
   if (!postForm) return null;
-  console.log("preview.jsが読み込まれました")
+  console.log("preview.jsが読み込まれました");
+
+  // input要素を取得
+  const fileField = document.querySelector('input[type="file"][name="post_form[image]"]');
+
+  // input要素で値の変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', function(e) {
+    console.log("input要素で値の変化が起きました");
+  });
 });
